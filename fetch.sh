9120a15e2b05eaec7839f83b83c1f361adaba5b2
Francis Begyn <francis@begyn.be>
2022-09-17 22:46:33 +0200

    update to new nfpm layout

--- fetch.sh
+++ fetch.sh
@@ -1,11 +1,5 @@
-#!/bin/env bash
+#! /usr/bin/env bash
 curl -fSL -o "./alertmanager-$1.linux-$2.tar.gz" \
   "https://github.com/prometheus/alertmanager/releases/download/v$1/alertmanager-$1.linux-$2.tar.gz"
-
 tar xzvf ./alertmanager-$1.linux-$2.tar.gz
-
-# get binaries
-mkdir ./bin
-cp ./alertmanager-$1.linux-$2/alertmanager ./bin/
-cp ./alertmanager-$1.linux-$2/amtool ./bin/
-cp ./alertmanager-$1.linux-$2/alertmanager.yml ./
+cp ./alertmanager-$1.linux-$2/* ./
--- package.sh
+++ package.sh
@@ -1,9 +1,11 @@
-#!/bin/env bash
+#! /usr/bin/env bash
 
 ./fetch.sh $1 $2
 
 sed "s/vx.y.z/v$1/g" ./nfpm.yaml > ./nfpm-temp.yaml
 
-nfpm pkg --config="nfpm-temp.yaml" --target="alertmanager_$1_$2.deb"
+nfpm pkg --packager deb --config="nfpm-temp.yaml"
+nfpm pkg --packager rpm --config="nfpm-temp.yaml"
 
-rm -rf ./bin ./alertmanager-$1.linux-$2 ./alertmanager-$1.linux-$2.tar.gz ./nfpm-temp.yaml
+rm -rf ./alertmanager ./alertmanager-$1.linux-$2 ./LICENSE ./NOTICE \
+	./alertmanager-$1.linux-$2.tar.gz ./nfpm-temp.yaml ./amtool ./alertmanager.yml
